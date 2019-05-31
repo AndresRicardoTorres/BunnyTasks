7cf3bb907798500dce94591d6e26bc327e1a3998
Dash Daemon <ricardo.torres.cto@gmail.com>
2019-05-30 20:55:22 -0500

    Version 1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,72 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatTableModule,
+  MatDividerModule,
+  MatSnackBarModule,
+  MatTreeModule,
+  MatRadioModule,
+  MatCheckboxModule,
+  MatDialogModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ListComponent as UserListComponent } from './users/list/list.component';
+import { ListComponent as TaskListComponent } from './tasks/list/list.component';
+import { DeleteDialogComponent } from './users/list/delete-dialog';
+
+const routes: Routes = [
+  { path: '', component: UserListComponent },
+  { path: ':id', component: UserListComponent }
+];
 
 @NgModule({
+  entryComponents: [DeleteDialogComponent],
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    TaskListComponent,
+    DeleteDialogComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatTreeModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDialogModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
